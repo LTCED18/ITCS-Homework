7f437789571e21e8cadc961aa939158b7f902213
LTCED18 <34748737+LTCED18@users.noreply.github.com>
2019-01-24 16:13:07 -0500

    HW3 v2

--- 2550 HW3.cpp
+++ 2550 HW3.cpp
@@ -1,33 +1,41 @@
-// 2550 HW3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include "pch.h"
 #include <iostream>
+#include <string>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
-void generateArray(int array[], int size);
-int seqSearch(const int list[], int listLength, int searchItem);
 
-const int ISIZE = 10;
+void generateArray(int array[], int size);
+int seqSearch(const int list[], int listLength, int searchItem, int index);
+void insertionSortArray(int list[], int listLength);
+void printArray(int list[], int size);
+
+const int ISIZE = 9;
 int main()
 {
 	int search;
-	srand(time(nullptr));
+    srand(time(nullptr));
 	int array[ISIZE] = { {'\0'} };
+	//int array2[ISIZE] = { 9, 2, 47, 1, 15, 23, 11, 36, 8 };
+	
 	
 	generateArray(array, ISIZE);
+
+	printArray(array, ISIZE);
+	
+	insertionSortArray(array, ISIZE);
+
+	printArray(array, ISIZE);
 
 	cout << "Enter search item: ";
 	cin >> search;
 	cout << endl;
 
-	seqSearch(array, 10, search);
-	/*for (int i = 0; i < 10; i++) {
-		cout << array[i] << " ";
-	}*/
-
+	seqSearch(array, 9, search, 0);
+	
 	system("pause");
 	return 0;
 
@@ -40,27 +48,46 @@ void generateArray(int array[], int size) {
 	}
 }
 
-int seqSearch(const int list[], int listLength, int searchItem)
-{
-	int loc;
-	bool found = false;
-	loc = 0;
-		if (list[loc] == searchItem && list[loc] < listLength)
-			found = true;
-		else
-			loc++;
-			seqSearch(list, listLength, searchItem);
-		    
-		    
+ void insertionSortArray(int list[], int listLength) {
 
-	if (found) {
-		cout << "We found " << list[loc] << endl;
-		return loc;
-	
-	}
-	else {
-		cout << "number was not found" << endl;
-		return -1;
+	int i = 1;
+	while (i < listLength) { // i increments the loop
 		
+		int j = i;
+
+		while (j >= 0 && list[j - 1] > list[j]) {
+			swap(list[j], list[j-1]);
+			j--;
+		}
+		
+		i++;
 	}
+
+}
+
+int seqSearch(const int list[], int listLength, int searchItem, int index)
+{
+	
+	if (index < listLength) {
+		if (list[index] != searchItem) {
+			seqSearch(list, listLength, searchItem, index + 1);
+		}
+		else if (list[index] == searchItem) {
+			cout << "We found " << list[index] << " at position " << index << endl;
+			return index;
+		}
+	} else {
+		cout << "Number Was Not Found" << endl;
+		return -1;
+	}
+
+
+}
+
+void printArray(int list[], int size) {
+	
+	for (int i = 0; i < ISIZE; i++) {
+		cout << list[i] << " ";
+	}
+	cout << endl << endl;
 }
